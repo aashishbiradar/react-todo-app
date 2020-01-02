72213ffdbaddf4e00263b75f8a0f17429727eb15
aashishbiradar <aashish.biradar@gmail.com>
2020-01-02 23:50:01 +0530

    Todos component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    ); 
+  }
 }
 
 export default App;
