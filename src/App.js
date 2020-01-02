1a39a4ed4a3059420998f171bb34a168dacac04c
aashishbiradar <aashish.biradar@gmail.com>
2020-01-03 00:29:10 +0530

    PropTypes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import './App.css';
 import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos : [
+      {
+        id : 1,
+        task : "Task 1",
+        completed : true
+      },
+      {
+        id : 2,
+        task : "Task 2",
+        completed : false
+      },
+      {
+        id : 3,
+        task : "Task 3",
+        completed : true
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos todos = {this.state.todos}/>
       </div>
     ); 
   }
