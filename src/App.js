c81099ffc9a09e17ffe8b977cef92df0dd2a3280
aashishbiradar <aashish.biradar@gmail.com>
2020-01-05 00:57:04 +0530

    added about us

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
 import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
+import uuid from 'uuid';
+import About from './components/pages/About';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   state = {
     todos : [
       {
-        id : 1,
+        id : uuid.v4(),
         title : "Task 1",
         completed : false
       },
       {
-        id : 2,
+        id : uuid.v4(),
         title : "Task 2",
         completed : false
       },
       {
-        id : 3,
+        id : uuid.v4(),
         title : "Task 3",
         completed : false
       }
@@ -39,12 +43,33 @@ class App extends Component {
     this.setState({todos: [...this.state.todos.filter(todo => todo.id != id)]});
   }
 
+  //add todo
+  addTodo = (title) => {
+    const newTodo = {
+      id : uuid.v4(),
+      title,
+      completed : false
+    }
+    this.setState({ todos : [...this.state.todos,newTodo]});
+  }
+
   render() {
     return (
-      <div className="App">
-        <Header/>
-        <Todos todos = {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header/>
+            <Route exact path="/" render={(props) => (
+                  <React.Fragment>
+                    <AddTodo addTodo={this.addTodo}/>
+                    <Todos todos = {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+                  </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About}/>
+          </div>
+        </div>
+      </Router>
     ); 
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <div style={{ textAlign : 'center' }}>
+                <h3>About Us</h3>
+                <p>This is Todo App built with react.js</p>
+            </div>
+        </React.Fragment>
+    )
+}
+
