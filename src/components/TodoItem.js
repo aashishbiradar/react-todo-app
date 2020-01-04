31daf477427c201a798d2132f6613dc8ec053f78
aashishbiradar <aashish.biradar@gmail.com>
2020-01-04 16:40:01 +0530

    added getStyle

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            textDecoration : this.props.todo.completed ? 'line-through':'none',
+            backgroundColor : '#f4f4f4',
+            borderBottom: '1px #ccc dotted',
+            padding : '10px'  
+        }
+    }
     render() {
         return (
-            <div>
-                <h1>{this.props.todo.task}</h1>
+            <div style={this.getStyle()}>
+                <p>{this.props.todo.task}</p>
             </div>
         )
     }
